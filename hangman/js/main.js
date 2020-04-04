bcd1ff55d6b1f75ed3f492fec46776656ed4a9e4
Tim <tdadyka@gmail.com>
2020-04-04 18:31:49 +0300

    v0.2

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -23,7 +23,7 @@ console.log(answerArray.join(' '));
 	var arr_ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
 	for (let i = 0; i < arr_ru.length; i++) {
 		let letter = arr_ru[i];
-		$('.letters').append('<input type="button" value="' + letter +'" class="key">');
+		$('.letters').append('<input type="button" value="' + letter +'" class="key able">');
 	};
 
 
@@ -31,6 +31,7 @@ console.log(answerArray.join(' '));
 	// <=================== ENDING ==============
 	var lose = function(){
 		console.log('You lose!');
+		alert('You lose!');
 	};
 
 	var victory = function(){
@@ -54,23 +55,147 @@ console.log(answerArray.join(' '));
 		console.log(answerArray);
 	};
 
+
+	var drw = 8;
+	var res = 0;
+	var lastRes = 0;
 	var result = function(){
-		let res = 0;
+		let wrong = true;
+		let check = 0;
 		for (var i = 0; i < wordComp.length; i++) {
 		 	if (wordComp[i] == answerArray[i]){
-		 		console.log('=======');
 		 		res++;
-		 		console.log(res);
 		 	};
 		};
-		if (res == wordComp.length) {
+		{check = res - lastRes};
+		if (check > 0){
+			wrong = false;
+		};
+		if (check == 0){
+			wrong = true;
+		};
+		{lastRes = res};
+		{res = 0};
+		if (wrong) {
+			drw--;
+		};
+		if (lastRes == wordComp.length) {
 			victory();
-		}
+		};
+		if (drw == 0){
+			lose();
+		};
+		console.log(drw + ' ' + wrong);
+		animate(drw);
 	};
 
 
+
+	// <===================== ANIMATION ================
+	var c = document.getElementById("ccc");
+	console.log(c);
+	var ctx = c.getContext("2d");
+	var x1 = (c.width / 10);
+	var y1 = (c.height / 15);
+	console.log(x1);
+	console.log(y1);
+
+	ctx.beginPath();
+	ctx.strokeStyle = '#fff';
+	ctx.lineWidth = 5;
+	ctx.lineCap = 'round';
+
+	ctx.moveTo(x1, y1 * 14);
+	ctx.lineTo(x1 * 7, y1 * 14);
+	ctx.moveTo(x1 * 3, y1 * 14);
+	ctx.lineTo(x1 * 3, y1 * 2);
+	ctx.lineTo(x1 * 7, y1 * 2);
+
+	ctx.stroke();
+	ctx.closePath();
+
+
+	var animate = function(draw){
+		let bodyWidth = 100;
+		switch(draw){
+			// ========= ROPE ==========
+			case 7:
+				ctx.beginPath();
+				ctx.moveTo(x1 * 7, y1 * 2);
+				ctx.lineTo(x1 * 7, y1 * 4);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= HEAD ==========			
+			case 6:
+				ctx.beginPath();
+				ctx.arc(x1 * 7, y1 * 5, x1, 0, Math.PI * 2);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= NECK ==========	
+			case 5:
+				ctx.beginPath();
+				ctx.moveTo(x1 * 7, y1 * 6);
+				ctx.lineTo(x1 * 7, (y1 * 6) + (y1 / 2));
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= BODY ==========	
+			case 4:
+				ctx.beginPath();
+				ctx.lineWidth = bodyWidth;
+				ctx.moveTo(x1 * 7, (y1 * 6) + (y1 / 2));
+				ctx.lineTo(x1 * 7, y1 * 9);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= L-HAND ==========	
+			case 3:
+				ctx.beginPath();
+				ctx.lineWidth = 5;
+				ctx.moveTo((x1 * 7) - (bodyWidth / 2), (y1 * 6) + (y1 / 2));
+				ctx.lineTo((x1 * 7) - bodyWidth, y1 * 9);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= R-HAND ==========	
+			case 2:
+				ctx.beginPath();
+				ctx.moveTo((x1 * 7) + (bodyWidth / 2), (y1 * 6) + (y1 / 2));
+				ctx.lineTo((x1 * 7) + bodyWidth, y1 * 9);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= L-LEG ==========	
+			case 1:
+				ctx.beginPath();
+				ctx.moveTo((x1 * 7) - (bodyWidth / 2), y1 * 9);
+				ctx.lineTo((x1 * 7) - (bodyWidth / 1.5), y1 * 11);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+			// ========= R-LEG ==========	
+			case 0:
+				ctx.beginPath();
+				ctx.moveTo((x1 * 7) + (bodyWidth / 2), y1 * 9);
+				ctx.lineTo((x1 * 7) + (bodyWidth / 1.5), y1 * 11);
+				ctx.stroke();
+				ctx.closePath();
+			break;
+
+		};
+	};
+	
+
+
+
+	// <=================== ON CLIC =================
 	$(function(){
 		$('.key').click(function(){
+			$(this).attr('disabled','true');
+			$(this).addClass('clicked');
+			$(this).removeClass('able');
 			console.log(this.value);
 			compare(this.value);
 			result();
